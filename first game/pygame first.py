a35ce2dda41bf8030ae9392156dba2ae2e6ab18a
Ruebeb07 <135453157+Ruebeb07@users.noreply.github.com>
2024-02-08 21:12:03 +0100

    Update pygame first.py

--- first game/pygame first.py
+++ first game/pygame first.py
@@ -6,11 +6,11 @@ pygame.mixer.init()
 #variable definitions
 resolution = [1920,1080] #resolution is done with a list so you can change it easier using code. This can be helpfull for resolution settings.
 screen = pygame.display.set_mode((resolution))
-playerY = 540 #defines the starting y position
-playerX = 960 #defines the starting x position
-playerHeight = 75
-playerWidht = 50
-player = pygame.Rect((playerX,playerY,playerWidht,playerHeight))
+playerY = resolution[1] / 2 #defines the starting y position
+playerX = resolution[0] / 2 #defines the starting x position
+playerHeight = 150
+playerWidht = 100
+player = pygame.Rect((playerX,playerY,playerWidht,playerHeight)) #REMOVE LATER!!!
 music_volume = 0.4 #defines the volume of the music
 music_active = False
 clock = pygame.time.Clock() #initializes the clock
@@ -25,9 +25,18 @@ try:
     original_background = pygame.image.load('Textures/StartScreen/Temp.png')
     background = pygame.transform.scale(original_background, resolution)
     location = "startscreen"
-#if loading fails it will print that in the console
-except pygame.error as e:
+#if loading fails it will print this in the console
+except:
     print("startscreen couldn't load")
+
+try:
+    original_character = pygame.image.load('Textures/Player/player.png')
+    character = pygame.transform.scale(original_character, (playerWidht,playerHeight))
+#if loading fails it will print that in the console
+except:
+    print("player texture couldn't load")
+player = character.get_rect()
+player.center = (playerX+(playerWidht/2), playerY+(playerHeight/2))
 
 run = True
 while run:
@@ -42,18 +51,18 @@ while run:
     
     if (key[pygame.K_a] == True) and (playerX > 0):
         player.move_ip(-movementspeed,0)
-        playerX -= 1
+        playerX -= movementspeed
     if (key[pygame.K_w] == True) and (playerY < resolution[1]):
         player.move_ip(0,-movementspeed)
-        playerY += 1
+        playerY += movementspeed
     if (key[pygame.K_s] == True) and (playerY > 0+playerHeight):
         player.move_ip(0,movementspeed)
-        playerY -= 1
+        playerY -= movementspeed
     if (key[pygame.K_d] == True) and (playerX < (resolution[0]-playerWidht)):
         player.move_ip(movementspeed,0)
-        playerX += 1
+        playerX += movementspeed
 
-    
+    screen.blit(character, player)
 
     #updates the screen so you can see changes
     pygame.display.update()
