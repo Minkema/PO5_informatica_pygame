0f21b5d178a6d216c2b3fc59d29919f2189c9975
Carsten <83281084+CarstenGeusebroek@users.noreply.github.com>
2024-03-10 13:26:51 +0100

    Main is nu helemaal schoon

--- first game/player.py
+++ first game/player.py
@@ -1,34 +1,25 @@
-import pygame, scenehandler
+import pygame, settings
 
-resolution = 0
-movementspeed = 4
-screen = 0
+resolution = settings.resolution
+screen = settings.screen
 
 #init all player values
-player = 0
-playerHeight = 0
-playerWidht = 0
+playerHeight = settings.playerHeight
+playerWidht = settings.playerWidht
 character = 0
 
 #movemenet values
-playerX = 0
-playerY = 0
+playerX = settings.playerX
+playerY = settings.playerY
+movementspeed = 4
 
-#Initiates some variables that are necessary for the player script
-def loadFirstValues():
-    global screen, resolution
-    resolution = scenehandler.resolution
-    screen = scenehandler.screen
+player = pygame.Rect((playerX,playerY,playerWidht,playerHeight))
 
-def loadPlayer(x, y, Widht, Height):
-    #Loads all the necessary variables inside the player script so that they can be used later
-    global player, playerHeight, playerWidht, playerX, playerY, character
-    player = pygame.Rect((x,y,Widht,Height))
-    playerHeight = Height
-    playerWidht = Widht
-    playerX = x
-    playerY = y
 
+def loadPlayer():
+
+    #trys to load texture on player
+    global character
     try:
         character_image = pygame.image.load('Textures/Player/player.png')
         character = pygame.transform.scale(character_image, (playerWidht,playerHeight))
@@ -46,6 +37,7 @@ def drawPlayer():
 def update_movement():
     key = pygame.key.get_pressed()
     global playerX, playerY
+
     if (key[pygame.K_a] == True) and (playerX > 0):
         player.move_ip(-movementspeed,0)
         playerX -= movementspeed
