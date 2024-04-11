3bac721d04e8f5d723ac6bac621046247eb597dc
Ruebeb07 <135453157+Ruebeb07@users.noreply.github.com>
2024-04-11 17:24:44 +0200

    Update scenehandler.py
    heb knoppies proberen toe te voegen, maar om de een of andere reden komen ze niet in beeld. geen errors, er gebeurt gwn niks. Zou iemand ernaar kunnen kijken
    
    side note:
    de "getal/1920 * resolution[0]" zorgt ervoor dat de verhoudingen op alle beeldschermen gelijk zijn.

--- first game/scenehandler.py
+++ first game/scenehandler.py
@@ -5,8 +5,6 @@ currentScene = "default"
 resolution = settings.resolution
 screen = settings.screen
 music_active = False
-button_surface = pygame.Surface((150, 50))
-button_rect = pygame.Rect(125, 125, 150, 50)
 
 astroids = []
 
@@ -32,17 +30,54 @@ def loadTestScene():
         print("startscreen couldn't load")
 
 def loadStartScene():
+    #defining variables
+    global background
+    global StartButton_img
+    global SettingsButton_img
+    global ExitButton_img
+    global Startbutton
+    global Settingsbutton
+    global Exitbutton
+
     #Loads the music in pygame so that it can be used later in the scene loop
     pygame.mixer.music.load('Audio\Startscreen\startscreen.mp3')
-
+    
     #Tries to load the specific background textures
-    global background
     try:
         background_image = pygame.image.load('Textures/StartScreen/homescreen.png')
         background = pygame.transform.scale(background_image, resolution)
     #if loading fails it will print that in the console
     except pygame.error as e:
-        print("startscreen couldn't load")
+        print("startscreen couldn't load: ", e)
+
+    #loading button textures
+    try:
+        StartButton_img = pygame.image.load('Textures/StartScreen/StartButton.png').convert_alpha()
+        SettingsButton_img = pygame.image.load('Textures/StartScreen/SettingsButton.png').convert_alpha()
+        ExitButton_img = pygame.image.load('Textures/StartScreen/ExitButton.png').convert_alpha()
+    except pygame.error as e:
+        print("button images couldn't load")
+    
+
+    #button class
+    class Button():
+        def __init__(self, x, y, image):
+            self.image = image
+            self.rect = self.image.get_rect()
+            self.rect.topleft = (x, y)
+
+        def draw(self):
+            #draw button on screen
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    #creating button instance
+    Startbutton = Button(((100/1920)*resolution[0]),((800/1920)*resolution[0]), StartButton_img)
+    Settingsbutton = Button(((100/1920)*resolution[0]),((600/1920)*resolution[0]), SettingsButton_img)
+    Exitbutton = Button(((100/1920)*resolution[0]),((400/1920)*resolution[0]), ExitButton_img)
+
+    Startbutton.draw()
+    Settingsbutton.draw()
+    Exitbutton.draw()
 
 last_execution_time = pygame.time.get_ticks()
 interval = 1000
@@ -80,5 +115,3 @@ def mainGameLoop():
 
         for astroid in astroids:
             astroid.draw()
-    
-    
