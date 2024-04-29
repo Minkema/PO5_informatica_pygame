2a61388d65aca9566f679fe0fd1dc291a1666904
Ruebeb07 <135453157+Ruebeb07@users.noreply.github.com>
2024-04-29 09:56:25 +0200

    Update settings.py
    logischere volgorde voor resolutie

--- first game/settings.py
+++ first game/settings.py
@@ -2,12 +2,12 @@ import pygame, scenehandler, textUI
 
 #these are lists with all the options that a setting can have
 difficultyList = ["Easy", "Medium", "Hard", "Antje"]
-resolutionList = [[2560,1440],[1920,1080],[1366,768],[1280,1024],[1024,768]]
+resolutionList = [[1024,768],[1280,1024],[1366,768],[1920,1080],[2560,1440]]
 fpsList = [120, 30, 60]
 volumeList = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 #Here you can find the different settings (Some of them can't be changed. That's why they don't have a list)
-resolution = resolutionList[1] #resolution is done with a list so you can change it easier using code. This can be helpfull for resolution settings.
+resolution = resolutionList[3] #resolution is done with a list so you can change it easier using code. This can be helpfull for resolution settings.
 playerY = resolution[1] / 2 #defines the starting y position
 playerX = resolution[0] / 2 #defines the starting x position
 playerWidht = (75/1920) * resolution[0]
